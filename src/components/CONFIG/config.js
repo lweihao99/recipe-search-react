e8db44f3dd0ee71ec90e6e23901a8d576767342f
Weihao Liu <weihaoliu99@gmail.com>
2023-12-03 22:09:27 +0100

    config

--- src/components/CONFIG/config.js
+++ src/components/CONFIG/config.js
@@ -1,5 +1,6 @@
 // https://forkify-api.herokuapp.com/v2
+// API_KEY = 82c8694f-26cc-41c2-96de-58926832c7d7
 
 const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
-
-export { API_URL };
+const API_KEY = "82c8694f-26cc-41c2-96de-58926832c7d7";
+export { API_URL, API_KEY };
