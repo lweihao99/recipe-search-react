e49044e1b887470fbc43f498b69f3262408dae1b
Weihao Liu <weihaoliu99@gmail.com>
2023-12-08 00:11:18 +0100

    content render

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/content.module.css";
+import icons from "../assets/icons.svg";
+import Fracty from "fracty";
 
-import { UserOutlined, BookOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  BookOutlined,
+  FieldTimeOutlined,
+  UsergroupAddOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
 
 import { Image, Button, Space } from "antd";
 
-function Content({ data }) {
+function Content({ data, updateServings }) {
   return (
     <div className={styles.content}>
+      {/* figure */}
       <div className={styles.imageContainer}>
         <Image
           style={{
@@ -18,20 +28,40 @@ function Content({ data }) {
           src={data.image}
         ></Image>
       </div>
+
+      {/* detail */}
       <div className={styles.recipe_detail}>
         <Space>
           <div className={styles.recipe_info}>
             <Space>
-              <span>{data.cookingTime}</span>
+              <FieldTimeOutlined />
+              <span className={styles.recipe_info_data}>
+                {data.cookingTime}
+              </span>
               <span>minutes</span>
             </Space>
           </div>
 
           <div className={styles.recipe_info}>
             <Space>
-              <span>{data.servings}</span>
+              <UsergroupAddOutlined />
+              <span className={styles.recipe_info_data}>{data.servings}</span>
               <span>servings</span>
             </Space>
+            <div className={styles.recipe_info_buttons}>
+              <button
+                className={styles.btn_tiny}
+                onClick={(e) => {
+                  e.preventDefault();
+                  updateServings(data.servings + 1);
+                }}
+              >
+                <PlusCircleOutlined />
+              </button>
+              <button className={styles.btn_tiny}>
+                <MinusCircleOutlined />
+              </button>
+            </div>
           </div>
         </Space>
 
@@ -43,22 +73,29 @@ function Content({ data }) {
         </Button>
       </div>
 
+      {/* render ingredients */}
       <div className={styles.recipe_ingredients}>
         <h2 className={styles.heading}>Recipe ingredients</h2>
         <ul className={styles.recipe_ingredient_list}>
-          <li className={styles.recipe_ingredient}>
-            <svg className={styles.recipe__icon}>
-              <use href="${icons}#icon-check"></use>
-            </svg>
-            <div className={styles.recipe__quantity}>200</div>
-            <div className={styles.recipe__description}>
-              <span className="recipe__unit">g</span>
-              {" grain sea salt "}
-            </div>
-          </li>
+          {data.ingredients &&
+            data.ingredients.map((item, index) => (
+              <li className={styles.recipe_ingredient} key={index}>
+                <svg className={styles.recipe__icon}>
+                  <use href={`${icons}#icon-check`}></use>
+                </svg>
+                <div className={styles.recipe__quantity}>
+                  {item.quantity ? Fracty(item.quantity) : ""}
+                </div>
+                <div className={styles.recipe__description}>
+                  <span className="recipe__unit">{item.unit}</span>
+                  {item.description}
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
 
+      {/* footer */}
       <div className={styles.recipe__directions}>
         <h2 className={styles.heading}>How to cook it</h2>
         <p className={styles.recipe__directions_text}>
