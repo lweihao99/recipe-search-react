cf1a1d64dcbe0d0e36d079287ef1242aa0b8281d
Weihao Liu <weihaoliu99@gmail.com>
2024-01-04 19:58:23 +0100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/login.module.css";
 import { Form, Button, Input } from "antd";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = (values) => {
-    if (values.name && values.password === "admin") {
-      navigate("/home");
+  const handleLogin = async () => {
+    try {
+      if (username === "admin " && password === "admin") navigate("/home");
+    } catch (error) {
+      console.error("Login Failed", error);
     }
   };
 
@@ -23,7 +28,10 @@ function Login() {
           name="name"
           rules={[{ required: true, message: "Please enter your username" }]}
         >
-          <Input placeholder="Please enter username"></Input>
+          <Input
+            placeholder="Please enter username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></Input>
         </Form.Item>
 
         {/* password input */}
@@ -32,7 +40,10 @@ function Login() {
           name="password"
           rules={[{ required: true, message: "Please enter your password" }]}
         >
-          <Input.Password placeholder="Please enter password"></Input.Password>
+          <Input.Password
+            placeholder="Please enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></Input.Password>
         </Form.Item>
 
         {/* login button */}
